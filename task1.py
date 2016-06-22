56f55b7ce2877e6775d7f8e0ce62d952db8a2828
Catbug33 <jorenka.ku@gmail.com>
2016-06-22 10:45:49 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,8 +2,6 @@ import numpy
 array = numpy.random.random_integers(0,9,(10,10,10))
 print array
 
-print '###'
-
 biggest_sum = 0;
 dimension = len(array)
 for i in range(dimension):
@@ -11,28 +9,28 @@ for i in range(dimension):
         for k in range(dimension):
 
             sum_current = 0
-            print 'XXX'
+           # print 'XXX'
             for x in range(0, dimension):
                 sum_current += array[i, j, x]
-                print x, 'index of x'
-                print array[i, j, x], '- number on[i, j, x] ', sum_current, ' - sum_current'
+                #print x, 'index of x'
+                #print array[i, j, x], '- number on[i, j, x] ', sum_current, ' - sum_current'
                 if sum_current > biggest_sum:
                     biggest_sum = sum_current
 
-            print 'YYY'
+            #print 'YYY'
             for y in range(0, dimension):
                 sum_current += array[i, y, k]
-                print y, 'index of y'
-                print array[i, y, k],'number on array[i, y, k] ', sum_current, 'sum_current'
+                # print y, 'index of y'
+                # print array[i, y, k],'number on array[i, y, k] ', sum_current, 'sum_current'
                 if sum_current > biggest_sum:
                     biggest_sum = sum_current
 
-            print 'ZZZ'
+            # print 'ZZZ'
             for z in range(0, dimension):
-                print array[z, j, k], 'number on array[z, j, k]'
+                # print array[z, j, k], 'number on array[z, j, k]'
                 sum_current += array[z, j, k]
-                print z, 'index of z                                    '
-                print sum_current, 'sum_current',
+                # print z, 'index of z                                    '
+                # print sum_current, 'sum_current',
                 if sum_current > biggest_sum:
                     biggest_sum = sum_current
 print 'here is it'
